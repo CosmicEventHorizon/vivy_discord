afb12453b37c6e36015a327e2a62e00a6914c964
Blood-Pirouette <bloodpirouette@tutanota.com>
2025-01-11 10:39:55 -0500

    Removed unnecessary console logging and improved error handling

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-
 /** Imports **/
 
 //import classes from discord.js
-const { Client, Collection, Events, 
-	GatewayIntentBits, VoiceState, ClientUser } = require('discord.js');
+const {
+	Client,
+	Collection,
+	Events,
+	GatewayIntentBits,
+	VoiceState,
+	ClientUser,
+} = require("discord.js");
 //import fs module for reading/writing files
-const fs = require('node:fs');
+const fs = require("node:fs");
 //import path module for working with direcotory paths
-const path = require('node:path');
+const path = require("node:path");
 //import config module containing necessary API keys
 const config = require("./config.json");
 //import search module containing necessary modules to search and play videos
-const searchCommand = require('./search.js');
+const searchCommand = require("./search.js");
 
 /** Variables  and Constants**/
 
@@ -28,125 +33,126 @@ let player = null;
 //GuildMessages: allow the bot to read server chat messages
 //Guild: allow the bot to respond to track server events
 const client = new Client({
-    intents: [GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds]
+	intents: [
+		GatewayIntentBits.GuildVoiceStates,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.Guilds,
+	],
 });
 //create a Collection object which will store bot commands
 client.commands = new Collection();
 
-
 /** Method calls **/
 
 //call once() to print Ready when the bot is emits a ready event
-client.once('ready', () => { 
-    console.log('Ready!');
+client.once("ready", () => {
+	console.log("Ready!");
 });
 
-  
 //listen to errors and warnings and print them
 client.on("error", console.error);
 client.on("warn", console.warn);
 
-//login to Discord 
+//login to Discord
 client.login(config.token);
 
-
 //call on() with the InteractionCreate event listener, triggers when the user interacts with the bot
-client.on(Events.InteractionCreate, async interaction => {
+client.on(Events.InteractionCreate, async (interaction) => {
 	//if the interaction is not a slash command, do nothing
-	if (!interaction.isChatInputCommand){return;}
+	if (!interaction.isChatInputCommand) {
+		return;
+	}
 
 	//get the commandName property from the interaction object and store it under "commandName"
 	const { commandName } = interaction;
 
 	//if the commandName is search then pass the interaction to the execute function in search module to open a selection menu
-	if (commandName === 'search') {
-	  await searchCommand.execute(interaction);
+	if (commandName === "search") {
+		await searchCommand.execute(interaction);
 	}
-	
+
 	//if the commandName is stop then call the stop() method of the player
-	if (commandName === 'stop') {
+	if (commandName === "stop") {
 		try {
 			//console.log(subprocessManagerArray)
 			player.stop();
-			await interaction.reply("Music stopped!")
+			await interaction.reply("Music stopped!");
 		} catch (error) {
-			console.log("Player doesn't exist")
-			await interaction.reply("Start a music :?")
-
+			console.log("Player doesn't exist");
+			await interaction.reply("Start a music :?");
 		}
 	}
 
 	//if the commandName is pause then call the pause() method of the player
-	if (commandName === 'pause') {
+	if (commandName === "pause") {
 		try {
 			//console.log(subprocessManagerArray)
 			player.pause();
-			await interaction.reply("Music paused!")
+			await interaction.reply("Music paused!");
 		} catch (error) {
-			console.log("Player doesn't exist")
-			await interaction.reply("Start a music :?")
+			console.log("Player doesn't exist");
+			await interaction.reply("Start a music :?");
 		}
 	}
 
 	//if the commandName is resume then call the unpause() method of the player
-	if (commandName === 'resume') {
+	if (commandName === "resume") {
 		try {
 			//console.log(subprocessManagerArray)
 			player.unpause();
-			await interaction.reply("Music resumed!")
+			await interaction.reply("Music resumed!");
 		} catch (error) {
-			console.log("Player doesn't exist")
-			await interaction.reply("Start a music :?")
+			console.log("Player doesn't exist");
+			await interaction.reply("Start a music :?");
 		}
 	}
-	
-
 });
 
 //call on() with the InteractionCreate event listener, triggers when the user interacts with the bot
-client.on(Events.InteractionCreate, async interaction => {
+client.on(Events.InteractionCreate, async (interaction) => {
 	//if the interaction is not a stringSelectMenu, do nothing
-	if (!interaction.isStringSelectMenu){
-		console.log("not a string select");
+	if (!interaction.isStringSelectMenu) {
 	}
-	console.log(interaction)
+
 	//get the customId property from the interaction object and store it under "customId"
-	const {customId} = interaction;
+	const { customId } = interaction;
 
 	//check that the user chooses an option
-	if (interaction.values && interaction.values.length > 0) {
-		selectedOption = interaction.values[0];
-	  } else {
-		console.log("No values selected.");
-	  }
-  
-	if (customId === 'searchResults') {
-	  	const replyContent = `You selected option: ${selectedOption}`;
- 		if (typeof subprocessPID === 'number'){
-				try{
-				console.log (typeof(subprocessManager))
-				process.kill(subprocessPID)
-				} catch (error){
-					console.log ("pid doesnt exist")
-				}
-	  	}
-		subprocessManager = searchCommand.playVideo(interaction, selectedOption)
-		console.log(subprocessManager)
-		subprocessManagerArray = await subprocessManager
-		subprocessPID = await subprocessManagerArray[0]
-		player = await subprocessManagerArray[1]
-		if (typeof subprocessPID === 'number'){
+	try {
+		if (interaction.values && interaction.values.length > 0) {
+			selectedOption = interaction.values[0];
+		} else {
+			console.log("No values selected.");
+		}
+	} catch (error) {
+		console.log(error);
+	}
+
+	if (customId === "searchResults") {
+		const replyContent = `You selected option: ${selectedOption}`;
+		if (typeof subprocessPID === "number") {
+			try {
+				//console.log (typeof(subprocessManager))
+				process.kill(subprocessPID);
+			} catch (error) {
+				console.log("pid doesnt exist");
+			}
+		}
+		subprocessManager = searchCommand.playVideo(interaction, selectedOption);
+		//console.log(subprocessManager)
+		subprocessManagerArray = await subprocessManager;
+		subprocessPID = await subprocessManagerArray[0];
+		player = await subprocessManagerArray[1];
+		if (typeof subprocessPID === "number") {
 			try {
 				await interaction.reply(replyContent);
 				console.log("Replied successfully!");
-			  } catch (error) {
+			} catch (error) {
 				console.error("Error replying to interaction:", error);
-			  }
+			}
 		}
-		if (typeof subprocessPID === 'string' || subprocessPID instanceof String){
+		if (typeof subprocessPID === "string" || subprocessPID instanceof String) {
 			await interaction.reply("Please enter voice chat");
 		}
 	}
-
 });
-
--- search.js
+++ search.js
@@ -1,8 +1,12 @@
 /** Imports **/
 
 //import classes from discord.js
-const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, 
-  ActionRowBuilder, SlashCommandBuilder } = require('discord.js');
+const {
+	StringSelectMenuBuilder,
+	StringSelectMenuOptionBuilder,
+	ActionRowBuilder,
+	SlashCommandBuilder,
+} = require("discord.js");
 //import youtube-search-api module for searching youtube
 const youtubesearchapi = require("youtube-search-api");
 
@@ -10,106 +14,117 @@ const youtubesearchapi = require("youtube-search-api");
 
 //string array containing the youtube titles
 const titleArray = [];
-//array containing the id of each youtube titles 
+//array containing the id of each youtube titles
 const idArray = [];
 //holds the StringSelectMenuOption objects with the id and title
 const optionsArray = [];
 //holds the relevant parts of the json response from youtube-search-api
-let jsonResult = '';
-
-
-
+let jsonResult = "";
 
 module.exports = {
-  playVideo,
+	playVideo,
 	data: new SlashCommandBuilder()
-		.setName('search')
-		.setDescription('Search Youtube for video')
-    .addStringOption(option =>
-      option.setName('keyword')
-        .setDescription('The keyword to search')
-        .setRequired(true)),
+		.setName("search")
+		.setDescription("Search Youtube for video")
+		.addStringOption((option) =>
+			option
+				.setName("keyword")
+				.setDescription("The keyword to search")
+				.setRequired(true)
+		),
 	async execute(interaction) {
-    optionsArray.length = 0;
-    titleArray.length = 0;
-    idArray.length = 0;
-    const keyword = interaction.options.getString('keyword')
-    await youtubeSearch(keyword);
-    for (let i = 0; i< titleArray.length; i++){
-      const options = new StringSelectMenuOptionBuilder()
-      .setLabel(titleArray[i])
-      .setValue(idArray[i])
+		optionsArray.length = 0;
+		titleArray.length = 0;
+		idArray.length = 0;
+		const keyword = interaction.options.getString("keyword");
+		await youtubeSearch(keyword);
+		for (let i = 0; i < titleArray.length; i++) {
+			const options = new StringSelectMenuOptionBuilder()
+				.setLabel(titleArray[i])
+				.setValue(idArray[i]);
 
-      optionsArray.push(options)
-    }
-    const select = new StringSelectMenuBuilder()
-			.setCustomId('searchResults')
-			.setPlaceholder('Search Results')
+			optionsArray.push(options);
+		}
+		const select = new StringSelectMenuBuilder()
+			.setCustomId("searchResults")
+			.setPlaceholder("Search Results")
 			.addOptions(optionsArray);
 
-    const row = new ActionRowBuilder()
-      .addComponents(select);
-  
-     await interaction.reply({
-        components: [row],
-      });
+		const row = new ActionRowBuilder().addComponents(select);
+
+		await interaction.reply({
+			components: [row],
+		});
 	},
 };
 
-    
 async function youtubeSearch(keyword) {
-        try {
-          const results = await youtubesearchapi.GetListByKeyword(keyword, true, 5, { });
-          const jsonItems = results.items;
-          console.log(jsonItems)
-          for (const item of jsonItems) {
-            const shortTitle = item.title
-            const shortTitleString = shortTitle.substring(0, 80);
-            titleArray.push(shortTitleString)
-            idArray.push(item.id)
-            jsonResult += item.title + item.id + ', ';
-          }
-        } catch (error) {
-          console.error(error);
-        }
+	try {
+		const results = await youtubesearchapi.GetListByKeyword(
+			keyword,
+			true,
+			5,
+			{}
+		);
+		const jsonItems = results.items;
+		//console.log(jsonItems)
+		for (const item of jsonItems) {
+			const shortTitle = item.title;
+			const shortTitleString = shortTitle.substring(0, 80);
+			titleArray.push(shortTitleString);
+			idArray.push(item.id);
+			jsonResult += item.title + item.id + ", ";
+		}
+	} catch (error) {
+		console.error(error);
+	}
 }
 
-async function playVideo(interaction , url) {
-  const { joinVoiceChannel, createAudioPlayer, createAudioResource, StreamType } = require('@discordjs/voice');
-  const ytdl = require('youtube-dl-exec')
-  const fixUrl = 'https://www.youtube.com/watch?v=' + url
-  const voiceChannel = interaction.member.voice.channel;
-  const { createReadStream } = require('node:fs');
-  const { join } = require('node:path');
-  let subprocess = null;
+async function playVideo(interaction, url) {
+	const {
+		joinVoiceChannel,
+		createAudioPlayer,
+		createAudioResource,
+		StreamType,
+	} = require("@discordjs/voice");
+	const ytdl = require("youtube-dl-exec");
+	const fixUrl = "https://www.youtube.com/watch?v=" + url;
+	const voiceChannel = interaction.member.voice.channel;
+	const { createReadStream } = require("node:fs");
+	const { join } = require("node:path");
+	let subprocess = null;
 
-  if (!voiceChannel) {
-    const voiceError = "error"
-    return voiceError
-  } else {
-    try {
-      const connection = joinVoiceChannel({
-        channelId: interaction.member.voice.channel.id,
-        guildId: interaction.message.guild.id,
-        adapterCreator: interaction.message.channel.guild.voiceAdapterCreator,
-      });
-      subprocess = ytdl.exec(fixUrl, {
-        o: '-',
-        q: '',
-        f: 'bestaudio',
-      }, { stdio: ['ignore', 'pipe', 'ignore'] })
-      player = createAudioPlayer();
-      const resource = createAudioResource(subprocess.stdout, {inputType: StreamType.Arbitrary,});
-      //console.log(subprocess)
-      player.stop()
-      player.play(resource)
-      connection.subscribe(player)
-      return [subprocess.pid, player];
-      } catch (error) {
-      console.log(error);
-      interaction.reply('There was an error playing the video.');
-    }
-  }
+	if (!voiceChannel) {
+		const voiceError = "error";
+		return voiceError;
+	} else {
+		try {
+			const connection = joinVoiceChannel({
+				channelId: interaction.member.voice.channel.id,
+				guildId: interaction.message.guild.id,
+				adapterCreator: interaction.message.channel.guild.voiceAdapterCreator,
+			});
+			subprocess = ytdl.exec(
+				fixUrl,
+				{
+					o: "-",
+					q: "",
+					f: "bestaudio",
+				},
+				{ stdio: ["ignore", "pipe", "ignore"] }
+			);
+			player = createAudioPlayer();
+			const resource = createAudioResource(subprocess.stdout, {
+				inputType: StreamType.Arbitrary,
+			});
+			//console.log(subprocess)
+			player.stop();
+			player.play(resource);
+			connection.subscribe(player);
+			return [subprocess.pid, player];
+		} catch (error) {
+			console.log(error);
+			interaction.reply("There was an error playing the video.");
+		}
+	}
 }
-
-
